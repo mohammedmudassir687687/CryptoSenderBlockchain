8ad9a14e4ed75783dcd33ed8e27c85190b14b01f
Mohammed Mudassir <mohammedmudassir687687@gmail.com>
2022-10-05 14:40:37 -0400

    isLoading state updated and dummyTransactions replaced

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0xbF51e85DbfA24948d15fb39838D08105799Ad683';
+export const contractAddress = '0x136a44Dfed624F9537BE8b1479CC0AcbEf0C32d9';
